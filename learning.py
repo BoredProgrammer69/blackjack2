88f40650ce28c65cbed76c6aa906ec7651bc3650
BoredProgrammer69 <clichename420@gmail.com>
2023-07-27 14:26:06 -0700

    modified:   learning.py

--- learning.py
+++ learning.py
@@ -7,7 +7,6 @@ count = 0
 top = 21
 wins = 0
 
-#a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t = 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 numbers = []
 
 def replay():
@@ -23,7 +22,7 @@ def check2():
 def bot2():
     global card1, card2, cards, count, top, wins
     print(str(cards))
-    while sum(cards) <= 18 and count == 0:
+    while sum(cards) <= 21 and count == 0:
         cards.append(random.randint(1, 11))
         count = count + 1
         time.sleep(1)
@@ -32,4 +31,23 @@ def bot2():
             count = count - 1
         if sum(cards) == 21:
             wins = wins + 1
-                
+
+def begin():
+    bot2()
+    check2()
+
+
+def whatever():
+    for i in range (80000):
+        print(str(cards))
+        while sum(cards) < 21:
+            count = count + 1
+        if count >= 1:
+            cards.append(random.randint(1, 11))
+            numbers.append[(sum(cards))]
+            sumnatioj = 0
+            for j in range (len(sum(cards))) - 1:
+                sumnatioj = sumnatioj + cards[j]
+            # numbers[sum(cards)] = numbers(sum(cards))
+                numbers[sumnatioj] = numbers[sumnatioj] + 1
+                count = count - 1   
