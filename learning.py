0e6c28370159d2510350d5a1533656c85c88e259
BoredProgrammer69 <clichename420@gmail.com>
2023-07-28 11:23:23 -0700

    modified:   learning.py

--- learning.py
+++ learning.py
@@ -7,17 +7,17 @@ count = 0
 top = 21
 wins = 0
 
-numbers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+numbers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-def replay():
-    bot2()
-    check2()
+# def replay():
+#     bot2()
+#     check2()
 
-def check2(): 
-    if sum(p1cards) > 21:
-        replay()
-    else:
-        return
+# def check2(): 
+#     if sum(p1cards) > 21:
+#         replay()
+#     else:
+#         return
 
 # def bot2():
 #     global card1, card2, cards, count, top, wins
@@ -32,19 +32,24 @@ def check2():
 #         if sum(cards) == 21:
 #             wins = wins + 1
 
-def begin():
-    bot2()
-    check2()
+
 
 
 def whatever():
     global card1, card2, cards, count, top, wins, sumnatioj
+    card1 = random.randint(1,11)
+    card2 = random.randint(1,11)
+    cards = []
+    cards.append(card1)
+    cards.append(card2)
+    count = 0
+    top = 21
+    wins = 0
     for i in range (80000):
         time.sleep(2)
         print(str(cards))
         while sum(cards) < 21:
-            count = count + 1 #todo add cards to hand here
-            print(count)
+            count = count + 1 
             cards.append(random.randint(1, 11))
             sumnatioj = 0
             for j in range (len((cards))- 1):
@@ -53,6 +58,32 @@ def whatever():
                 count = count - 1   
             file = open('numbers.txt','w')
             file.write(str(numbers))
+            if sum(cards) == 21:
+                count = 0
+                wins = wins + 1
+                print("Win with cards: " + str(cards))
+                file = open('numbers.txt','w')
+                file.write("/nWIN! With cards " + str(cards))   
+                whatever()
+        if sum(cards) == 21:
+                count = 0
+                wins = wins + 1
+                print("Win with cards: " + str(cards))
+                file = open('numbers.txt','w')
+                file.write("/nWIN! With cards " + str(cards))   
+                whatever()
+        if sum(cards) > 21:
+            count = count - 1
+            # lent = len(cards)
+            sumnatioj = 0
+            for j in range (0, len((cards))-1) :
+                sumnatioj = sumnatioj + cards[j]
+            # sumnatioj = sumnatioj + cards[j]
+            numbers[sumnatioj] = numbers[sumnatioj] - 1
+            print("Loss with: " + str(cards))
+            file = open('numbers.txt','w')
+            file.write(str(numbers))
+            whatever()
         if count >= 1:
             cards.append(random.randint(1, 11))
             sumnatioj = 0
@@ -60,5 +91,8 @@ def whatever():
                 sumnatioj = sumnatioj + cards[j]
                 numbers[sumnatioj] = numbers[sumnatioj] + 1
                 count = count - 1   
+                file = open('numbers.txt','w')
+                file.write(str(numbers))
+                
 
 whatever()
